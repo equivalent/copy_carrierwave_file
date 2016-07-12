f3153eaff871383e92f1e9413f925590cbfbd1e9
flavio <flavioveloso@incbio.com>
2016-07-12 16:24:19 +0100

    Add support to carrierwave aws

--- lib/copy_carrierwave_file/copy_file_service.rb
+++ lib/copy_carrierwave_file/copy_file_service.rb
@@ -33,7 +33,7 @@ module CopyCarrierwaveFile
           set_file_for_local_storage
         when 'CarrierWave::Storage::Fog'
           set_file_for_remote_storage
-        when 'CarrierWave::Storage::aws'
+        when 'CarrierWave::Storage::AWS'
           set_file_for_remote_storage
         else
           raise UnknowStorage
