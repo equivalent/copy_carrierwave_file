c82d02539135d3c5ec49541fcdd98cb554288eb1
flavio <flavioveloso@incbio.com>
2016-07-15 14:10:11 +0100

    Add small refactor

--- lib/copy_carrierwave_file/copy_file_service.rb
+++ lib/copy_carrierwave_file/copy_file_service.rb
@@ -31,9 +31,7 @@ module CopyCarrierwaveFile
         case original_resource_mounter.send(:storage).class.name
         when 'CarrierWave::Storage::File'
           set_file_for_local_storage
-        when 'CarrierWave::Storage::Fog'
-          set_file_for_remote_storage
-        when 'CarrierWave::Storage::AWS'
+        when 'CarrierWave::Storage::Fog', 'CarrierWave::Storage::AWS'
           set_file_for_remote_storage
         else
           raise UnknowStorage
